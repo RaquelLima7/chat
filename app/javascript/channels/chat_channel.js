af1cbda792062b7e9d0e1423f549884b7b872519
RaquelLima7 <raquellimadev@gmail.com>
2021-10-01 20:39:02 -0300

    chat room

--- app/javascript/channels/chat_channel.js
+++ app/javascript/channels/chat_channel.js
@@ -1,6 +1,6 @@
 import consumer from "./consumer"
 
-consumer.subscriptions.create("ChatChannel", {
+const chatChannel = consumer.subscriptions.create("ChatChannel", {
   connected() {
     console.log("Connected to the chat room!");
   },
@@ -10,10 +10,26 @@ consumer.subscriptions.create("ChatChannel", {
   },
 
   received(data) {
-    // Called when there's incoming data on the websocket for this channel
+    $('#messages').append(`<p>${data.sender}: ${data.message}</p>`);
+    $('html, body').animate({scrollTop:$(document).height()}, 0);
   },
 
-  speak: function() {
-    return this.perform('speak');
+  speak: function(data) {
+    return this.perform('speak', data);
   }
 });
+
+$(document).on('turbolinks:load', function () {
+  $("#message_form").on('submit', function(e){
+    e.preventDefault();
+    let data = {
+      message: $('#message_to_sent').val(),
+      sender: $('#sender').val()
+    }
+
+    if (data.message.length > 0 && data.sender.length > 0) {
+      chatChannel.speak(data);
+      $('#message_to_sent').val('')
+    }
+  });
+})
